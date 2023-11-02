0d3910ccc3922092da6f031619a990fa94953255
Thanatcha Bizcuit <thanatcha@bizcuitsolution.com>
2023-11-02 23:03:06 +0700

    feat: implement css post component

--- Template-and-Styling-test/src/Component/PostItem/PostItem.tsx
+++ Template-and-Styling-test/src/Component/PostItem/PostItem.tsx
@@ -1,11 +1,22 @@
 import { Post } from "../../Config/posts";
+import "./PostItem.scss";
 
 interface IPost {
   post: Post;
 }
 
 const PostItem: React.FC<IPost> = ({ post }) => {
-  return <div className="post-item">{post.body}</div>;
+  return (
+    <div className="post">
+      <div className="body">
+        <img className="img" src={post.image_url} alt="" />
+        <div className="main-content">
+          <div className="title">{post.title}</div>
+          <div className="text">{post.body} </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostItem;
