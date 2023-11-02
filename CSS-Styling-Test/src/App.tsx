f812172f50276b658fb5b3d163750708d4c7eb47
Thanatcha Bizcuit <thanatcha@bizcuitsolution.com>
2023-11-02 22:06:37 +0700

    feat: create first question

--- CSS-Styling-Test/src/App.tsx
+++ CSS-Styling-Test/src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="maqe">
+      <div className="horizontal-line" />
+      <div className="circle-element-group">
+        <div className="character-circle">
+          <div className="character-circle-inner ">M</div>
+        </div>
+        <div className="character-circle">
+          <div className="character-circle-inner ">A</div>
+        </div>
+        <div className="character-circle">
+          <div className="character-circle-inner ">Q</div>
+        </div>
+        <div className="character-circle">
+          <div className="character-circle-inner ">E</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
